71dd71541472ac111e7371c77d6e4a46c7339abc
MotherDeveloper <a.o.shelestova@gmaol.com>
2013-03-20 12:20:32 +0700

    first is found in x, brez bad written

--- src/FIT_0204_Shelestova/Plotter/FunctionBuilder.java
+++ src/FIT_0204_Shelestova/Plotter/FunctionBuilder.java
@@ -1,6 +1,10 @@
 package FIT_0204_Shelestova.Plotter;
 
+import FIT_0204_Shelestova.Common.GUIStandarts;
+
+import static java.lang.Math.PI;
 import static java.lang.Math.abs;
+import static java.lang.Math.nextAfter;
 
 public class FunctionBuilder {
     private int xNull;
@@ -16,12 +20,33 @@ public class FunctionBuilder {
             this.x = x;
             this.y = y;
             dF = model.func(x, y);
-
+            pw = model.getPanelWidth();
 
         }
-        public int x;
-        public int y;
+
+        private int x;
+        private int y;
         public double dF;
+        public int pw;
+
+        public void setX(int x) {
+            this.x = x;
+            dF = model.func(x,y);
+        }
+
+        public void setY(int y) {
+            this.y = y;
+            dF = model.func(x,y);
+        }
+        public void draw(){
+            bits[(y+yNull) * pw + x + xNull] = GUIStandarts.figureColor;
+        }
+        public boolean isPainted (){
+            if (bits[(y+yNull) * pw + x + xNull] == GUIStandarts.figureColor)
+                return true;
+            else
+                return false;
+        }
     }
     private static PlotterModel model;
     private int[] bits;
@@ -38,51 +63,60 @@ public class FunctionBuilder {
         Point p = new Point(300, 0);
     }
     private void doIt(){
-        binarySearch();
+        Point firstPoint = xSearchFirstPoint();
+        drawByBresenchem(firstPoint);
+        //System.out.println(firstPoint.x + " : " + firstPoint.y + " = " + firstPoint.dF);
     }
-    private Point binarySearch(){
-        Point firstPoint = null;
-        firstPoint = yBinarySearch(new Point(model.getX1(), yNull), new Point(model.getX1(), 0));
-        System.out.println(firstPoint.x + " : " + firstPoint.y + " == " + firstPoint.dF);
-        if(firstPoint != null) return firstPoint;
-        return firstPoint;
-    }
-    private Point xBinarySearch(Point left, Point right){
-        if (left.dF == 0) return left;
-        if (right.dF == 0) return right;
-        int dx = (right.x - left.x)/2;
-        if (dx == 0){
-            if(left.dF < right.dF) return left;
-            else return right;
+
+    private Point xSearchFirstPoint(){
+        Point startPoint = new Point(-xNull, model.getY1());
+        Point currPoint = new Point(-xNull, model.getY1());
+
+        while (startPoint.dF * currPoint.dF > 0){
+            if(currPoint.x <= xNull){
+                currPoint.setX(currPoint.x + 1);
+            }
+            else {
+                break;
+            }
         }
-        if (abs(left.dF) > abs(right.dF)){
-            return xBinarySearch(new Point(left.x + dx, left.y), right);
+        Point preCurrPoint = new Point(currPoint.x - 1, currPoint.y);
+
+        if (abs(preCurrPoint.dF) < abs(currPoint.dF)){
+            return preCurrPoint;
         }
         else {
-            return xBinarySearch(right, new Point(right.x - dx, right.y));
+            return currPoint;
+        }
+    }
+
+    private void drawByBresenchem(Point startPoint){
+        Point firstPoint = startPoint;
+        int j = 50;
+        while(j > 0){
+            startPoint.draw();
+            Point[] points = new Point[8];
+            points[0] = new Point(startPoint.x - 1, startPoint.y - 1);
+            points[1] = new Point(startPoint.x - 1, startPoint.y);
+            points[2] = new Point(startPoint.x - 1, startPoint.y + 1);
+            points[3] = new Point(startPoint.x, startPoint.y -1);
+            points[4]= new Point(startPoint.x, startPoint.y + 1);
+            points[5] = new Point(startPoint.x + 1, startPoint.y + 1);
+            points[6] = new Point(startPoint.x + 1, startPoint.y - 1);
+            points[7]= new Point(startPoint.x + 1, startPoint.y);
+            Point next = points[0];
+            for (int i = 1; i < 8; i++){
+                if (points[i].isPainted() ){
+                    continue;
+                }
+                else if( points[i].dF < next.dF){
+                    next = points[i];
+                }
+            }
+            startPoint = next;
+            j--;
         }
 
     }
-    private Point yBinarySearch(Point top, Point bottom){
-        System.out.println("top " + top.y + " : " + top.dF + " ::: " + "bottom " + bottom.y + " : " + bottom.dF);
-        if (top.dF == 0) return top;
-        if (bottom.dF == 0) return bottom;
-        int dy = (top.y - bottom.y)/2;
-        if (dy == 0){
-            if(top.dF < bottom.dF) return top;
-            else return bottom;
-        }
-        if (top.dF * bottom.dF < 0){
-            if (abs(top.dF) > abs(bottom.dF)){
-                return yBinarySearch(new Point(top.x, top.y - dy), bottom);
-            }
-            else {
-                return yBinarySearch(top, new Point(bottom.x, bottom.y + dy));
-            }
-        }
-        else{
-            if (abs(top.dF) > abs(bottom.dF)) return bottom;
-            else return top;
-        }
-    }
+
 }
